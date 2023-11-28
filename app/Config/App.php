9325f9e6aa9d705f25d4b922ed9f6f8cfa8c456a
srliath <santos.jonathan.jj@gmail.com>
2023-11-28 14:57:30 -0300

    last with the key

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/jb/public/';
+    public string $baseURL = 'https://jblogistic.com.br/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
